d0a8f957e9fff2d0776c3888731149cb7e73a216
adeybabs <adeybabs.aa@gmail.com>
2025-03-20 01:57:50 +0100

    backend url config

--- src/main/java/com/project/fedfaxe/controller/Oauth2Controller.java
+++ src/main/java/com/project/fedfaxe/controller/Oauth2Controller.java
@@ -2,6 +2,7 @@ package com.project.fedfaxe.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Authentication", description = "OAuth2 Authentication APIs")
 public class Oauth2Controller {
 
+    @Value("${app.backend.url}")
+    private String backendUrl;
+
     @Operation(
             summary = "Google OAuth2 Login",
             description = "Redirects user to Google for authentication. After successful login, user is redirected to the configured frontend URL."
     )
     @GetMapping("/login/google")
     public String googleLogin() {
-        return "Redirect to: http://localhost:8087/oauth2/authorization/google";
+        return "Redirect to: " + backendUrl + "/oauth2/authorization/google";
     }
 }
